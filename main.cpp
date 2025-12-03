e0166daeb5b1c624d8bb33c8b9202d46f63e6c33
MisterE1234 <MisterE1234@outlook.com>
2025-12-03 09:59:59 -0800

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -29,7 +29,15 @@ int main() {
 //returns: an integer value
 int sum_ascii(string str){
     int sum = 0;
+    int count = 0;
+    if (debug){
+        cout << "Calculating ascii sum for: " << str << endl;
+    }
     for (char c : str){
+        if(debug){
+            cout << " Char " << count++ << ": " << c << " Ascii: " << (int) c << endl;
+
+        }
         sum += (int) c;
     }
 
@@ -40,6 +48,6 @@ int sum_ascii(string str){
 These targets are present in the dataset and can be used for testing:
 536B9DFC93AF 
 1DA9D64D02A0
-666D109AA22E ascii sum = 
+666D109AA22E ascii sum = 683
 E1D2665B21EA
 */
