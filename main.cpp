13a409ade1f3dac9bf720cd3d60c67069d836b9c
MisterE1234 <MisterE1234@outlook.com>
2025-12-06 15:50:01 -0800

    5th Commit

--- main.cpp
+++ main.cpp
@@ -9,12 +9,14 @@
 using namespace std;
 
 //universal variables:
-bool debug = false, debug_precise  = false; 
+bool debug = false, debug_precise  = false;  int ENTRY_AMOUNT = 100;
 
-int sum_ascii(string);
+int gen_hash_index(string);
 
 int main() {
-    int total_ascii = 0;
+    map<int, list<string>> hash_table; //hash table: the int is the ascii value of the string
+
+    
     string temp_str;
 
     ifstream iFile("lab-37-data-2.txt");
@@ -29,38 +31,45 @@ int main() {
         cout << "File Opened\n";
     }
 
-    while (!iFile.eof()){
+    for (int i = 0; i < ENTRY_AMOUNT; i++){
         getline(iFile, temp_str);
-        total_ascii += sum_ascii(temp_str);
+        hash_table[gen_hash_index(temp_str)].push_back(temp_str);
+        
     }
     iFile.close();
-    cout << "Total Ascii Sum: " << total_ascii << endl;
+    if(debug){
+        cout << "File Closed\n";
+    }
+
+    //displaying the has_table contents:
+    for (auto pair : hash_table){
+        cout << "ACII Value: " << pair.first << " -> ";
+        for (auto str : pair.second){
+            cout << str << ", ";
+        }
+    }
 
 
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 55;
-    cout << b << endl;
-    cout << (char) b << endl;
+    
     return 0;
 }
 
-//sum_ascii() makes an sum of all ascii value in a given string
+//gen_hash_index() makes an sum of all ascii value in a given string
 //requires:  a string variable
 //returns: an integer value
-int sum_ascii(string str){
+int gen_hash_index(string str){
     int sum = 0;
-    int count = 0;
+    int count = 0; //for debug perposes
     if (debug){
         cout << "Calculating ascii sum for: " << str << endl;
     }
+    //using a range based for loop to iterate through the string.
     for (char c : str){
         if(debug_precise){
             cout << " Char " << count++ << ": " << c << " Ascii: " << (int) c << endl;
 
         }
-        sum += (int) c;
+        sum += (int) c; //add the ascii value of the current character to the ascii sum
     }
 
     return sum;
